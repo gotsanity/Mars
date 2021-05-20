b5517ee48d54d9e22ceb55d317b9857bcb19ea0d
jesse harlan <gotsanity@gmail.com>
2021-05-20 13:25:19 -0700

    Role Administration added

---
+++ Mars/Controllers/RoleAdminController.cs
@@ -0,0 +1,154 @@
+﻿using Mars.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mars.Controllers
+{
+    [Authorize(Roles = "Admins")]
+    public class RoleAdminController : Controller
+    {
+        private RoleManager<IdentityRole> _roleManager;
+        private UserManager<IdentityUser> _userManager;
+
+        public RoleAdminController(RoleManager<IdentityRole> roleMgr, UserManager<IdentityUser> usrManager)
+        {
+            _roleManager = roleMgr;
+            _userManager = usrManager;
+        }
+
+        [HttpGet]
+        public ViewResult Index() => View(_roleManager.Roles);
+
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Required] string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+
+            return View(name);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+
+            if (role != null)
+            {
+                IdentityResult result = await _roleManager.DeleteAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "No role found.");
+            }
+
+            return View("Index", _roleManager.Roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            List<IdentityUser> members = new List<IdentityUser>();
+            List<IdentityUser> nonMembers = new List<IdentityUser>();
+
+            foreach (IdentityUser user in _userManager.Users)
+            {
+                var list = await _userManager.IsInRoleAsync(user, role.Name) ? members : nonMembers;
+                list.Add(user);
+            }
+
+            return View(new RoleEditModel
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RoleModificationModel model)
+        {
+            IdentityResult result;
+
+            if (ModelState.IsValid)
+            {
+                foreach(string userID in model.IdsToAdd ?? new string[] { })
+                {
+                    IdentityUser user = await _userManager.FindByIdAsync(userID);
+
+                    if (user != null)
+                    {
+                        result = await _userManager.AddToRoleAsync(user, model.RoleName);
+
+                        if (!result.Succeeded)
+                        {
+                            AddErrorsFromResult(result);
+                        }
+                    }
+                }
+
+                foreach (string userID in model.IdsToDelete ?? new string[] { })
+                {
+                    IdentityUser user = await _userManager.FindByIdAsync(userID);
+
+                    if (user != null)
+                    {
+                        result = await _userManager.RemoveFromRoleAsync(user, model.RoleName);
+
+                        if (!result.Succeeded)
+                        {
+                            AddErrorsFromResult(result);
+                        }
+                    }
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return await Edit(model.RoleId);
+            }
+        }
+    }
+}
---
+++ Mars/Models/ViewModels/RoleEditModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mars.Models.ViewModels
+{
+    public class RoleEditModel
+    {
+        public IdentityRole Role { get; set; }
+        public List<IdentityUser> Members { get; set; }
+        public List<IdentityUser> NonMembers { get; set; }
+    }
+}
